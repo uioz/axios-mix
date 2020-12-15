674509c6a6e9858a55fe9d892ca7078eeefe6bd9
ASCll <hzxbpld@163.com>
2020-12-15 09:37:24 +0800

    WIP:

--- src/index.ts
+++ src/index.ts
@@ -10,48 +10,135 @@ export interface Options {
   retry: any;
 }
 
-export interface AxiosMixInstance {
-  (config: AxiosRequestConfig): AxiosPromise;
-  (url: string, config?: AxiosRequestConfig): AxiosPromise;
+/**
+ * 拦截器定义
+ */
+export interface AxiosMixRequestConfig extends AxiosRequestConfig {
+  beforeRequest?: any;
+  afterResponse?: any;
+  failHandler?: any;
+  errorHandler?: any;
+}
+
+/**
+ * 利用新的拦截器定义重写 axios 方法定义
+ */
+export interface AxiosMixInstance extends AxiosInstance {
+  (config: AxiosMixRequestConfig): AxiosPromise;
+  (url: string, config?: AxiosMixRequestConfig): AxiosPromise;
   request<T = any, R = AxiosResponse<T>>(
-    config: AxiosRequestConfig
+    config: AxiosMixRequestConfig
   ): Promise<R>;
   get<T = any, R = AxiosResponse<T>>(
     url: string,
-    config?: AxiosRequestConfig
+    config?: AxiosMixRequestConfig
   ): Promise<R>;
   delete<T = any, R = AxiosResponse<T>>(
     url: string,
-    config?: AxiosRequestConfig
+    config?: AxiosMixRequestConfig
   ): Promise<R>;
   head<T = any, R = AxiosResponse<T>>(
     url: string,
-    config?: AxiosRequestConfig
+    config?: AxiosMixRequestConfig
   ): Promise<R>;
   options<T = any, R = AxiosResponse<T>>(
     url: string,
-    config?: AxiosRequestConfig
+    config?: AxiosMixRequestConfig
   ): Promise<R>;
   post<T = any, R = AxiosResponse<T>>(
     url: string,
     data?: any,
-    config?: AxiosRequestConfig
+    config?: AxiosMixRequestConfig
   ): Promise<R>;
   put<T = any, R = AxiosResponse<T>>(
     url: string,
     data?: any,
-    config?: AxiosRequestConfig
+    config?: AxiosMixRequestConfig
   ): Promise<R>;
   patch<T = any, R = AxiosResponse<T>>(
     url: string,
     data?: any,
-    config?: AxiosRequestConfig
+    config?: AxiosMixRequestConfig
   ): Promise<R>;
 }
 
+/**
+ * 实例方法扩展定义
+ */
+export interface ExtendAxiosInstance extends AxiosMixInstance {
+  extend: any;
+  inject: any;
+  eject: any;
+}
+
+type ProxyKeys =
+  | "request"
+  | "get"
+  | "delete"
+  | "head"
+  | "options"
+  | "post"
+  | "put"
+  | "patch";
+
+// 重写方法签名
+const ProxyKeySet = new Set<ProxyKeys>([
+  "request",
+  "get",
+  "delete",
+  "head",
+  "options",
+  "post",
+  "put",
+  "patch",
+]);
+
+type ExtendedKeys = "extend" | "inject" | "eject";
+
+// 扩展方法签名
+const ExtendedKeySet = new Set<ExtendedKeys>(["extend", "inject", "eject"]);
+
+function isProxyKeySet(key: any): key is ProxyKeys {
+  return ProxyKeySet.has(key);
+}
+
+function isExtendedKeySet(key: any): key is ProxyKeys | ExtendedKeys {
+  return ExtendedKeySet.has(key);
+}
+
 function AxiosMix(axios: AxiosInstance, options?: Options) {
-  return new Proxy(axios, {
-    
+  const interceptorsQueue = {
+    request: [],
+    get: [],
+    delete: [],
+    head: [],
+    options: [],
+    post: [],
+    put: [],
+    patch: [],
+  };
+
+  axios.interceptors.request.use(function (config: AxiosMixRequestConfig) {
+    return config;
+  });
+
+  axios.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {}
+  );
+
+  return new Proxy<ExtendAxiosInstance>(axios as any, {
+    get(target, key) {
+      if (isExtendedKeySet(key)) {
+        return function () {};
+      }
+
+      // 透明代理
+      // @ts-ignore
+      return target[key];
+    },
   });
 }
 
--- test/index.ts
+++ test/index.ts
@@ -5,38 +5,36 @@ import MockAdapter from "axios-mock-adapter";
 const axios = AxiosMix(Axios);
 const mock = new MockAdapter(axios);
 
-mock.onGet("/users").reply(200, {
+mock.onGet("/users").reply(500, {
   users: [{ id: 1, name: "John Smith" }],
 });
 
-axios.interceptors.request.use(
-  function (config) {
-    return config;
-  },
-  function (error) {
-    console.log("request error");
-    debugger;
-  }
-);
+// axios.interceptors.request.use(
+//   function (config) {
+//     return config;
+//   },
+//   function (error) {
+//     console.log("request error");
+//     debugger;
+//   }
+// );
 
 axios.interceptors.response.use(
   function (response) {
-    debugger
+    debugger;
     return response;
   },
   function (error) {
-    console.log("response error");
+    return Promise.resolve("hello world");
     debugger;
   }
 );
 
-axios
-  .get("/users")
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(() => {});
-
-axios("/test").then(function (response) {
+axios.get("/users").then(function (response) {
+  debugger;
   console.log(response.data);
 });
+
+// axios("/users").then(function (response) {
+//   console.log(response.data);
+// });
